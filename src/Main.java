1912cdb59513e8630f1a3f70f80f5022421b836f
detayaa <drugochenko.taya@mail.ru>
2022-08-25 19:39:20 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+    System.out.println("Первое задание");
+    int first = 340957;
+    byte second = 23;
+    long third = -38538475L;
+    float fourth = 2398.42039f;
+    double fifth = 2.3456764346764;
+    System.out.println("Значение переменной first с типом int равно " + first);
+    System.out.println("Значение переменной second с типом byte равно " + second);
+    System.out.println("Значение переменной third с типом long равно " + third);
+    System.out.println("Значение переменной fourth с типом float равно " + fourth);
+    System.out.println("Значение переменной fifth с типом double равно " + fifth);
+
 
     }
 }
